c996c8e15e6dbe63ffdfbc990bd6b44dd712d0b5
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-04-09 01:34:42 -0500

    adding some initial scrapers

---
+++ scrapers/nomineeScrape.py
@@ -0,0 +1,25 @@
+#### nomineeScrape.py ####
+# James Whang, 4/9/2015
+
+import urllib2
+import io
+from bs4 import BeautifulSoup
+
+
+writeFile = open('result.csv', 'w') # File to write the results to
+
+################## MAIN LOOP #######################
+for i in range(10000):
+	html = urllib2.urlopen('http://www.nobelprize.org/nomination/archive/show_people.php?id=' + str(i)).read()
+
+	soup = BeautifulSoup(html)
+
+	entries = soup.find_all('td')
+
+	# filled entries have 6 rows in the table 
+	if len(entries) == 6:
+		firstName = str(entries[1]).replace('<td>', '').replace('</td>', '') # strip the html tags
+		lastName = str(entries[3]).replace('<td>', '').replace('</td>', '')
+		writeFile.write(str(i) + ":" + firstName + ', ' + lastName + '\n')
+
+writeFile.close()
