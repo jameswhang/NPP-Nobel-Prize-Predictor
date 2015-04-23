4876328121d47cb1171fa4a340720c6daf93e870
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-04-22 23:26:02 -0500

    lint code

--- scrapers/nomineeScrape.py
+++ scrapers/nomineeScrape.py
@@ -1,26 +1,31 @@
-#### nomineeScrape.py ####
-#### James Whang, 4/9/2015
-#### Scraper for Nobel Prize Nominees Names
+#!/usr/bin/python
+
+# nomineeScrape.py ####
+# James Whang, 4/9/2015
+# Scraper for Nobel Prize Nominees Names
 
 import urllib2
-import io
 from bs4 import BeautifulSoup
 
-writeFile = open('result.csv', 'w') # File to write the results to
+writeFile = open('result.csv', 'w')  # File to write the results to
 
-################## MAIN LOOP #######################
+#        MAIN LOOP          #
 for i in range(10000):
-	print 'Checking ' + str(i) + '...'
-	html = urllib2.urlopen('http://www.nobelprize.org/nomination/archive/show_people.php?id=' + str(i)).read()
+    print 'Checking ' + str(i) + '...'
+    html = urllib2.urlopen(
+        'http://www.nobelprize.org/nomination/archive/show_people.php?id='
+        + str(i)).read()
 
-	soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html)
 
-	entries = soup.find_all('td')
+    entries = soup.find_all('td')
 
-	# filled entries have 6 rows in the table 
-	if len(entries) == 6:
-		firstName = str(entries[1]).replace('<td>', '').replace('</td>', '') # strip the html tags
-		lastName = str(entries[3]).replace('<td>', '').replace('</td>', '')
-		writeFile.write(str(i) + ":" + firstName + ', ' + lastName + '\n')
+    # filled entries have 6 rows in the table
+    if len(entries) == 6:
+        # strip html tags
+        firstName = str(entries[1]).replace('<td>', '').replace('</td>', '')
+        lastName = str(entries[3]).replace('<td>', '').replace('</td>', '')
+        # write to output
+        writeFile.write(str(i) + ":" + firstName + ', ' + lastName + '\n')
 
 writeFile.close()
